5ed6867b954e240cabfb8359d28c2fd5a7650387
Tsai0409 <138881929+Tsai0409@users.noreply.github.com>
2025-03-04 22:30:50 +0800

    Update yolox_train_one_fold.py

--- yolox_train_one_fold.py
+++ yolox_train_one_fold.py
@@ -104,7 +104,7 @@ fold = args.fold
 config = args.config
 cfg = eval(args.config)()
 # absolute_path = /kaggle/working
-print(absolute_path)
+print(cfg.absolute_path)
 cfg.train_df.path = cfg.absolute_path + '/' + cfg.train_df.path  # train 照片路徑；ex:/kaggle/temp/axial_all_images/2767326159___223384___5.png
 cfg.test_df.path = cfg.absolute_path + '/' + cfg.test_df.path
 
