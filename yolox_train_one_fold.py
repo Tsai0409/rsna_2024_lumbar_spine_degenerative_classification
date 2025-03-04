95c5f5ff63d49f18d79ae098f6d404246ae9391f
Tsai0409 <138881929+Tsai0409@users.noreply.github.com>
2025-03-04 22:50:24 +0800

    Update yolox_train_one_fold.py

--- yolox_train_one_fold.py
+++ yolox_train_one_fold.py
@@ -178,7 +178,8 @@ import os
 import sys
 sys.path.append("/kaggle/working/duplicate/src/YOLOX")  # 我加
 os.chdir('/kaggle/working/duplicate/src/YOLOX')  # 我加
-train_str = f'PYTHONPATH=/kaggle/working/duplicate/src/YOLOX python tools/train.py -f {config_path} -d 1 -b {cfg.batch_size} --fp16 -o -c {cfg.pretrained_path}'
+# train_str = f'PYTHONPATH=/kaggle/working/duplicate/src/YOLOX python tools/train.py -f {config_path} -d 1 -b {cfg.batch_size} --fp16 -o -c {cfg.pretrained_path}'
+os.environ["PYTHONPATH"] = "/kaggle/working/duplicate/src/YOLOX:" + os.environ.get("PYTHONPATH", "")
 from yolox.exp import Exp as MyExp  # 用到 YOLOX/yolox
 
 class Exp(MyExp):
