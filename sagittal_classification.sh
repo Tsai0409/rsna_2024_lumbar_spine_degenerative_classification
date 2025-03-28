a710df0deb3c65a6c30ab56599846c3009ee9fd8
Tsai0409 <138881929+Tsai0409@users.noreply.github.com>
2025-03-28 23:59:53 +0800

    Update sagittal_classification.sh

--- sagittal_classification.sh
+++ sagittal_classification.sh
@@ -20,18 +20,18 @@ configs=(
     "rsna_saggital_mil_spinal_crop_x03_y05" 
     "rsna_saggital_mil_spinal_crop_x03_y07" 
 
-    # "rsna_saggital_mil_ss_crop_x03_y05_96" 
-    # "rsna_saggital_mil_ss_crop_x03_y07_96" 
-    # "rsna_saggital_mil_ss_crop_x03_y2_96" 
-    # "rsna_saggital_mil_ss_crop_x1_y07_96" 
+    "rsna_saggital_mil_ss_crop_x03_y05_96" 
+    "rsna_saggital_mil_ss_crop_x03_y07_96" 
+    "rsna_saggital_mil_ss_crop_x03_y2_96" 
+    "rsna_saggital_mil_ss_crop_x1_y07_96" 
     
-    # "rsna_saggital_mil_nfn_crop_x07_y1_v2" 
-    # "rsna_saggital_mil_nfn_crop_x15_y1_v2" 
-    # "rsna_saggital_mil_nfn_crop_x03_y1_v2" 
-    # "rsna_saggital_mil_nfn_crop_x05_y05_v2"
+    "rsna_saggital_mil_nfn_crop_x07_y1_v2" 
+    "rsna_saggital_mil_nfn_crop_x15_y1_v2" 
+    "rsna_saggital_mil_nfn_crop_x03_y1_v2" 
+    "rsna_saggital_mil_nfn_crop_x05_y05_v2"
     )
 # folds=(0 1 2 3 4)
-folds=(0 1)
+folds=(0)
 
 # 遍歷配置和摺疊數進行訓練與預測
 for config in "${configs[@]}"
