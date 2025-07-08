ab49189091b2a9683c1979a09abdd192f846c307
Tsai0409 <138881929+Tsai0409@users.noreply.github.com>
2025-07-08 21:05:03 +0800

    Update preprocess_for_sagittal_stage2.py

--- preprocess_for_sagittal_stage2.py
+++ preprocess_for_sagittal_stage2.py
@@ -23,7 +23,7 @@ for fold in range(1):
     test[pred_cols] = np.mean(preds, 0)  # 將 test_fold0、test_fold1 各個欄位取平均；對所有 config 的預測結果按 axis=0 求均值，這表示對同一筆資料，不同模型（或不同設定）的預測結果取平均
     test[pred_cols] = sigmoid(test[pred_cols]).astype(float)  # 轉換為 0-1 的數字(做正規化)
     spinal = test.copy()
-    spinal.to_csv('spinal.csv', index=False)
+    # spinal.to_csv('spinal.csv', index=False)
 
     
     targets = ['l1_right_neural', 'l2_right_neural', 'l3_right_neural', 'l4_right_neural', 'l5_right_neural', 'l1_left_neural', 'l2_left_neural', 'l3_left_neural', 'l4_left_neural', 'l5_left_neural']
@@ -40,7 +40,7 @@ for fold in range(1):
     test[pred_cols] = np.mean(preds, 0)
     test[pred_cols] = sigmoid(test[pred_cols]).astype(float)
     nfn = test.copy()
-    nfn.to_csv('nfn.csv', index=False)
+    # nfn.to_csv('nfn.csv', index=False)
 
     df = spinal.merge(nfn[['path']+pred_cols], on='path')
     df.to_csv('df.csv', index=False)
@@ -48,7 +48,7 @@ for fold in range(1):
     # fold_df = pd.read_csv(f'{WORKING_DIR}/csv_train/preprocess_4/train_with_fold.csv').drop_duplicates('study_id')[['fold', 'study_id']]  # 篩選 DataFrame 只保留 fold 和 study_id 這兩個欄位，丟棄其他欄位
     fold_df = pd.read_csv(f'{WORKING_DIR}/csv_train/preprocess_holdout_4/train_with_fold_holdout_test.csv').drop_duplicates('study_id')[['fold', 'study_id']]
     # fold_df = pd.read_csv(f'{WORKING_DIR}/csv_train/preprocess_holdout_4/train_with_fold_holdout.csv', low_memory=False).drop_duplicates('study_id')[['fold', 'study_id']]
-    fold_df.to_csv('fold_df.csv', index=False)
+    # fold_df.to_csv('fold_df.csv', index=False)
 
     # df.merge(fold_df, on='study_id').to_csv(f'input/train_for_sagittal_level_cl_v1_for_train_spinal_nfn_fold{fold}.csv', index=False)
     df.merge(fold_df, on='study_id').to_csv(f'train_for_sagittal_level_cl_v1_for_train_spinal_nfn_fold{fold}.csv', index=False)  # 這個產生的用意是什麼？
