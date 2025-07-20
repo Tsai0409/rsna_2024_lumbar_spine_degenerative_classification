ce6090c5667f04c2394b09120267f4468c61f0de
Tsai0409 <138881929+Tsai0409@users.noreply.github.com>
2025-07-20 22:52:54 +0800

    Update preprocess_for_sagittal_classification.py

--- preprocess_for_sagittal_classification.py
+++ preprocess_for_sagittal_classification.py
@@ -18,10 +18,11 @@ WORKING_DIR="/kaggle/working/duplicate"
 config = 'rsna_10classes_yolox_x'
 box_cols = ['x_min', 'y_min', 'x_max', 'y_max']
 # tr = pd.read_csv('input/train_with_fold.csv')
-tr = pd.read_csv(f'{WORKING_DIR}/csv_train/preprocess_4/train_with_fold.csv')  # 所有狀態的資訊
+tr = pd.read_csv(f'{WORKING_DIR}/csv_train/preprocess_4/train_with_fold.csv')  
+tr = pd.read_csv(f'{WORKING_DIR}/csv_train/preprocess_holdout_4/train_with_fold_holdout.csv')  # 所有狀態的資訊
 # oof = pd.concat([pd.read_csv(f'results/{config}/oof_fold{fold}.csv') for fold in range(5)])
-oof = pd.concat([pd.read_csv(f'{WORKING_DIR}/results/{config}/oof_fold{fold}.csv') for fold in range(2)])  # version-34 (sagittal 中 valid 預測出來的 bounding box 資訊)
-oof.to_csv('/kaggle/working/oof.csv')  # 我加 → Dataframe1
+oof = pd.concat([pd.read_csv(f'{WORKING_DIR}/results/{config}/oof_fold{fold}.csv') for fold in range(1)])  # version-34 (sagittal 中 valid 預測出來的 bounding box 資訊)
+oof.to_csv('oof.csv')  # 我加 → Dataframe1
 # test = pd.read_csv(f'results/wbf/{config}.csv')
 test = pd.read_csv(f'{WORKING_DIR}/results/wbf/{config}.csv')  # wbf/rsna_10classes_yolox_x.csv 經過整理過後的 bounding box (用 test 的資料)
 test['study_id'] = test.path.apply(lambda x: int(x.split('/')[-1].split('___')[0]))
@@ -29,7 +30,7 @@ test['series_id'] = test.path.apply(lambda x: int(x.split('/')[-1].split('___')[
 test = test[~test['study_id'].isin(oof.study_id)]  # 將在 oof 中有出現的 study_id 全部移除
 t2_ids = tr[tr.series_description_y == 'Sagittal T2/STIR'].series_id  # 留下 Spinal
 test = test[test['series_id'].isin(t2_ids)]
-test.to_csv('/kaggle/working/test.csv')  # 我加 → Dataframe2
+test.to_csv('test.csv')  # 我加 → Dataframe2
 
 dfs = []
 for i, idf in test.groupby('study_id'):  # 每個 study_id 對應 只能有一個 series_id
